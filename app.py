c0f1e5d87113fe0d2870996a3d5e0723403c00e2
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-02 21:10:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = input("enter operation: eg. +, -, *, /: ")
+
+if c == '+':
+    print(a + b)    
+elif c == '-':
+    print(a - b)    
+elif c == '*':
+    print(a * b)
+elif c == '/':
+    if b != 0:
+        print(a / b)
+    else:
+        print("Error: Division by zero is not allowed.")
