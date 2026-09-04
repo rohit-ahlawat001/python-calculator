044e0db8d3cf00412db061e34024b1680ec40f3a
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-04 09:45:20 +0530

    finding even odd number using input value

--- even_odd.py
+++ even_odd.py
@@ -3,3 +3,10 @@ if number % 2 == 0:
     print(f"{number} is an even number.")
 else:  
     print(f"{number} is an odd number.")
+
+# Now getting even odd number from user input
+userNumber = int(input("Enter a number to ceck even or odd: "))
+if userNumber % 2 == 0:
+    print(f"{userNumber} is an even number.")
+else:
+    print(f"{userNumber} is an odd number.")
