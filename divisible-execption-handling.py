09afeb183156b09a8a3480faa40cbb3be104b5a2
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-23 23:22:08 +0530

    write the code to throw error divisible by the zero

--- divisible-execption-handling.py
+++ divisible-execption-handling.py
@@ -1,20 +1,24 @@
 # cresting the program for the exctption handlingDivisible by Zero
 
-def checkDivisionError(a, b):
-    c = None
-    try:
-        c = a/b
-    except ZeroDivisionError:
-        raise ZeroDivisionError("Kindly enter the vlaid values")
-    finally:
-        print(c)
-        return c
+# def checkDivisionError(a, b):
+#     c = None
+#     try:
+#         c = a/b
+#     except ZeroDivisionError:
+#         raise ZeroDivisionError("Kindly enter the vlaid values")
+#     finally:
+#         print(c)
+#         return c
     
-checkDivisionError(10, 2)
+# checkDivisionError(10, 2)
 
 def throwError(valOne, valTwo):
     valThree = None
-try:
-    valThree = valOne / valTwo
-except ZeroDivisionError:
-    raise ZeroDivisionError("Kindly add the valid number")
+    try:
+        valThree = valOne / valTwo
+        print(valThree)
+        return valThree
+    except ZeroDivisionError:
+        raise ZeroDivisionError("Kindly add the valid number")
+    
+throwError(5, 0)
