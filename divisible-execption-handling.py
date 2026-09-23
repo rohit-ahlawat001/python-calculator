4a69ae081f2349b5ac6acbe66098555ddd964d3f
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-23 22:59:24 +0530

    did some improvements in program

--- divisible-execption-handling.py
+++ divisible-execption-handling.py
@@ -1,11 +1,13 @@
 # cresting the program for the exctption handlingDivisible by Zero
 
-def checkDivisionError(a, c):
+def checkDivisionError(a, b):
     c = None
-try:
-    c = a/b
-except ZeroDivisionError:
-    raise ZeroDivisionError("Kindly enter the vlaid values")
-finally:
-    print(c)
-    return c
+    try:
+        c = a/b
+    except ZeroDivisionError:
+        raise ZeroDivisionError("Kindly enter the vlaid values")
+    finally:
+        print(c)
+        return c
+    
+checkDivisionError(10, 2)
